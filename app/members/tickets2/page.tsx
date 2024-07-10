ef175b43695162f296298a0325eaea44e07b812d
JeffZuo953 <jeffordszuo@gmail.com>
2024-07-10 14:04:27 +0800

    update

---
+++ app/members/tickets2/page.tsx
@@ -0,0 +1,126 @@
+import React from 'react';
+
+import Link from 'next/link';
+import { type Metadata } from 'next';
+
+import Master from '@components/Layout/Master';
+import Section from '@components/Section/Section';
+import Heading from '@components/Heading/Heading';
+import ButtonGroup from '@components/Button/ButtonGroup';
+import ButtonGroupItem from '@components/Button/ButtonGroupItem';
+
+const Ticket = (): React.JSX.Element => (
+  <div className='ticket-item'>
+    <div className='item-right'>
+      <h2>敬请期待</h2>
+      <p>敬请期待</p>
+      <span className='material-symbols-outlined'>qr_code_2</span>
+      <strong>592778</strong>
+      <span className='up-border'></span>
+      <span className='down-border'></span>
+    </div>
+    <div className='item-left'>
+      <h5>谷艺凌香港演唱会-外场远区</h5>
+      <p>
+        <span className='material-symbols-outlined'>event</span>
+        TBD
+      </p>
+      <p>
+        <span className='material-symbols-outlined'>apartment</span>
+        Hong Kong 亚洲国际博览馆
+      </p>
+      <div className='actions'>
+        <Link href='/members/tickets' title='Download tickets'>
+          <span className='material-symbols-outlined'>download</span>
+        </Link>
+        <Link href='/members/tickets' title='Send tickets'>
+          <span className='material-symbols-outlined'>forward_to_inbox</span>
+        </Link>
+      </div>
+    </div>
+  </div>  
+)
+
+
+const Page = (): React.JSX.Element => (
+  <Master>
+    <Section className='white-background'>
+      <div className='container'>
+        <div className='center'>
+          <Heading type={1} color='gray' text='我的门票' />
+          <p className='gray form-information'>
+            您可以在任何时候访问您从本页购买的机票。您可以下载或发送您的票。
+            请注意: 您将无法在本页看到已经结束或取消的活动的门票。
+          </p>
+          <div className='button-container'>
+            <ButtonGroup color='gray'>
+              <ButtonGroupItem url='members/tickets' text='我的门票' active />
+              <ButtonGroupItem url='members/account' text='我的账户' />
+            </ButtonGroup>
+          </div>
+        </div>
+      </div>
+    </Section>
+
+    <Section className='white-background'>
+      <div className='container'>
+        <Ticket/>
+        <Ticket/>
+        <Ticket/>
+        <Ticket/>
+        <Ticket/>
+
+        <div className='ticket-item'>
+          <div className='item-right'>
+            <h2>21</h2>
+            <p>September</p>
+            <span className='material-symbols-outlined'>qr_code_2</span>
+            <strong>21EX9P</strong>
+            <span className='up-border'></span>
+            <span className='down-border'></span>
+          </div>
+          <div className='item-left'>
+            <h5>Lisa Crazy Hourse Show</h5>
+            <p>
+              <span className='material-symbols-outlined'>event</span>
+              TBD
+            </p>
+            <p>
+              <span className='material-symbols-outlined'>apartment</span>
+              Royal Albert Hall
+            </p>
+            <div className='actions'>
+              <Link href='/members/tickets' title='Download tickets'>
+                <span className='material-symbols-outlined'>download</span>
+              </Link>
+              <Link href='/members/tickets' title='Send tickets'>
+                <span className='material-symbols-outlined'>forward_to_inbox</span>
+              </Link>
+            </div>
+          </div>
+        </div>
+      </div>
+    </Section>
+  </Master>
+);
+
+const title = '我的门票';
+const canonical = 'https://modern-ticketing.com/members/tickets';
+const description = 'Modern ticketing is a modern ticketing solution';
+
+export const metadata: Metadata = {
+  title,
+  description,
+  keywords: 'modern ticketing',
+  alternates: { canonical },
+  openGraph: {
+    title,
+    description,
+    url: canonical,
+    type: 'website',
+    siteName: 'Modern Ticketing',
+    images: 'https://modern-ticketing.com/logo192.png',
+  },
+};
+
+export default Page;
